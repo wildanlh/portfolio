40c15354a7d6ce11662d36be30c05c23a02dc47f
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-01-26 17:23:44 +0700

    Fix Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -35,7 +35,7 @@ export const NavBar = () => {
         <Router>
             <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
                 <Container>
-                    <Navbar.Brand href="/">
+                    <Navbar.Brand href="#">
                         <img src={logo} alt="Logo" />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" >
