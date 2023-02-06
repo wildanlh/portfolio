1aa39642436aa8a76df9608649d0cd18dbc41886
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-02-06 16:39:11 +0700

    Update about me

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,21 +1,20 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
-import './css/Banner.css';
+import "./css/Banner.css";
 
 export const Banner = () => {
-
-  const openInNewTab = url => {
-    window.open(url, '_blank', 'noopener,noreferrer');
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
   };
 
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
   const toRotate = ["Software Engineer", "Web Designer", "UI/UX Designer"];
@@ -26,33 +25,37 @@ export const Banner = () => {
       tick();
     }, delta);
 
-    return () => { clearInterval(ticker) };
-  }, [text])
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
 
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
 
     if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
+      setDelta((prevDelta) => prevDelta / 2);
     }
 
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
+      setIndex((prevIndex) => prevIndex - 1);
       setDelta(period);
-    } else if (isDeleting && updatedText === '') {
+    } else if (isDeleting && updatedText === "") {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
       setIndex(1);
       setDelta(500);
     } else {
-      setIndex(prevIndex => prevIndex + 1);
+      setIndex((prevIndex) => prevIndex + 1);
     }
-  }
+  };
 
   return (
     <section className="banner" id="home">
@@ -60,32 +63,58 @@ export const Banner = () => {
         <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
                   <span className="tagline">Hi! I'm Wildan Luqmanul Hakim</span>
-                  <h1>{`I'm`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software Engineer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                  <h1>
+                    {`I'm`}{" "}
+                    <span
+                      className="txt-rotate"
+                      dataPeriod="1000"
+                      data-rotate='[ "Software Engineer", "Web Designer", "UI/UX Designer" ]'
+                    >
+                      <span className="wrap">{text}</span>
+                    </span>
+                  </h1>
                   <span>About Me</span>
                   <p>
-                    Dedicated student, I have good work and communication at teams,  i always to learn from mistakes to make better ones, experience in Website Development, Android Application Development and UI/UX Design.
-                    I am looking for experience at intership to improve my skills and connection with new people in the scope of work. I will perform the best of me for the company.
+                    Dedicated student, I have good work and communication at
+                    teams, I always to learn from mistakes to make better ones,
+                    experienced in Website Development, Android Application
+                    Development and UI/UX Design. I am looking for experience at
+                    intership to improve my skills and connection with new
+                    people in the scope of work. I will perform the best of me
+                    for the company.
                   </p>
-                  <button onClick={() => openInNewTab('https://youtu.be/vx64SjqJ9HA')}>
+                  <button
+                    onClick={() => openInNewTab("https://youtu.be/vx64SjqJ9HA")}
+                  >
                     Video Introduction
                     <ArrowRightCircle size={25} />
                   </button>
-                </div>}
+                </div>
+              )}
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                >
                   <img src={headerImg} alt="Header Img" />
-                </div>}
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
